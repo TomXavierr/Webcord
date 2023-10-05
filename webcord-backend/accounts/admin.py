bdc88506b410f2714c5669a219e9002cc8fa246f
Tom Xavier <tomxavieronthego@gmail.com>
2023-10-05 10:16:10 +0530

    ignore file modified

--- webcord-backend/accounts/admin.py
+++ webcord-backend/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-from .models import FriendList, FriendRequest,UserAccount
+from .models import UserAccount
 
 class AccountAdmin(UserAdmin):
 	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff')
@@ -15,28 +15,3 @@ class AccountAdmin(UserAdmin):
 
 
 admin.site.register(UserAccount, AccountAdmin)
-
-class FriendListAdmin(admin.ModelAdmin):
-    list_filter = ['user']
-    list_display = ['user']
-    search_fields = ['user']
-    readonly_fields = ['user',]
-
-    class Meta:
-        model = FriendList
-
-
-admin.site.register(FriendList, FriendListAdmin)
-
-
-class FriendRequestAdmin(admin.ModelAdmin):
-    list_filter = ['sender', 'receiver']
-    list_display = ['sender', 'receiver',]
-    search_fields = ['sender__username', 'receiver__username']
-    readonly_fields = ['id',]
-
-    class Meta:
-        model = FriendRequest
-
-
-admin.site.register(FriendRequest, FriendRequestAdmin)
