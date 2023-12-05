e3b74a06b895cce9e7693019cf7d6f6131036cc5
Tom Xavier <tomxavieronthego@gmail.com>
2023-12-05 16:50:46 +0530

    updated channel layers to redis

--- webcord-backend/webcord/settings.py
+++ webcord-backend/webcord/settings.py
@@ -274,8 +274,17 @@ DJOSER = {
 
 
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
